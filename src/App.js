86baedcbdcbdbe152e95789b1908e4da40f6048b
Raian-Siddique <raian0197@gmail.com>
2020-09-04 00:09:35 +0600

    error handle

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     fetch("https://restcountries.eu/rest/v2/all")
       .then((res) => res.json())
       .then((data) => setCountries(data))
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error)); // the website will run with error
   }, []);
   return (
     <div className="App">
